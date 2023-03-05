94973f5fc4bc7b1282597b7bf3787bcb89e93319
fumtas1k <fumta01a7@gmail.com>
2023-03-05 23:50:55 +0900

    [chage]private api

--- lib/cdk-workshop-lambda-stack.ts
+++ lib/cdk-workshop-lambda-stack.ts
@@ -1,12 +1,89 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { EndpointType, LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class CdkWorkshopLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // VPC
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "publicSubnet",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    // セキュリティグループ
+    const ec2SecurityGroup = new ec2.SecurityGroup(this, "Ec2Sg", {
+      vpc,
+      allowAllOutbound: true
+    });
+    const vpcEndpointSecurityGroup = new ec2.SecurityGroup(this, "VpcEndpointSg", {
+      vpc,
+    });
+    vpcEndpointSecurityGroup.addIngressRule(ec2SecurityGroup, ec2.Port.allTraffic());
+
+    // IAMロール
+    const ec2Role = new iam.Role(this, "ec2Role", {
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")],
+      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
+    });
+
+    // VPCエンドポイント
+    const privateVpcEndpoint = new ec2.InterfaceVpcEndpoint(this, "PrivateApiVpc", {
+      vpc,
+      service: ec2.InterfaceVpcEndpointAwsService.APIGATEWAY,
+      subnets: { subnets: vpc.publicSubnets },
+      securityGroups: [vpcEndpointSecurityGroup],
+      open: false,
+    });
+
+    // API Gatewayポリシー
+    const apiPolicy = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          principals: [new iam.AnyPrincipal],
+          actions: ["execute-api:Invoke"],
+          resources: ["execute-api:/*"],
+          effect: iam.Effect.DENY,
+          conditions: {
+            StringNotEquals: {
+              "aws:SourceVpce": privateVpcEndpoint.vpcEndpointId
+            }
+          }
+        }),
+        new iam.PolicyStatement({
+          principals: [new iam.AnyPrincipal],
+          actions: ["execute-api:Invoke"],
+          resources: ["execute-api:/*"],
+          effect: iam.Effect.ALLOW
+        }),
+      ]
+    });
+
+    // EC2インスタンス
+    const ec2Instance = new ec2.Instance(this, "Ec2Instance", {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+        cpuType: ec2.AmazonLinuxCpuType.X86_64,
+      }),
+      securityGroup: ec2SecurityGroup,
+      role: ec2Role,
+    });
+
 
     // lambdaの設定
     const lambda = new NodejsFunction(this, 'lambda', {
@@ -15,12 +92,11 @@ export class CdkWorkshopLambdaStack extends Stack {
       runtime: Runtime.NODEJS_16_X,
     });
 
-    // api gatewayの設定
-    const api = new LambdaRestApi(this, 'RestAPI', {
+    // private API Gateway
+    const privateApi = new LambdaRestApi(this, 'privateApi', {
+      endpointTypes: [EndpointType.PRIVATE],
       handler: lambda,
-      proxy: false,
+      policy: apiPolicy,
     });
-    const hello = api.root.addResource('cdk-workshop-lambda');
-    hello.addMethod('GET');
   }
 }
